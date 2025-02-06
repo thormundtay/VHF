3bfdd0bd14713b2431239fa8e67be5f6494ac514
thormundtay <105705777+thormundtay@users.noreply.github.com>
2025-02-06 20:44:27 +0800

    Reduce VHF speed to 1MHz for development
    Currently, the reference C code used to create stream has an approximate
    20% failure rate. Curiously, the data that is being written out looks
    very similar to the rust code's output, which is always failing.
    
    Somehow, the issue is completely resolved when dropping the speed by
    half, which contradicts the project in its very beginning.
    
    Perhaps a kernel regression? OpenSUSE Leap 6.4.0-150600.23.30-default
    kernel.

--- VHF-rs/src/runner/process/mod.rs
+++ VHF-rs/src/runner/process/mod.rs
@@ -66,7 +66,7 @@ impl VHF {
             let mut buf_write = BufWriter::new(self.raw_handle.try_clone().map_err(Error::Io)?);
             buf_write.write(b"clockinit; adcinit;").map_err(Error::Io)?;
             buf_write.write(b"config 16; param 0;").map_err(Error::Io)?;
-            buf_write.write(b"config 1; param 4;").map_err(Error::Io)?; // skips 4 samples
+            buf_write.write(b"config 1; param 9;").map_err(Error::Io)?; // skips 9 samples
             buf_write.write(b"config 2; param 0;").map_err(Error::Io)?; // Gain parameter = 0
             buf_write.write(b"config 3; param 0;").map_err(Error::Io)?; // debug param = 0
             buf_write.write(b"skip; skip;").unwrap();
