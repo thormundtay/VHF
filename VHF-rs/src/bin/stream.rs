2e7eb320472f17b57a64c0445b9f8db29a0f220c
thormundtay <105705777+thormundtay@users.noreply.github.com>
2025-02-07 17:24:15 +0800

    Add rudimentary mmap fetch streaming
    This example code is able to be executed once with data parity to the C
    implementation. However, second run with this rust executable leads to
    the wrong result, in which
    a) Cleaning up with clear-FIFO fixes the issue or;
    b) Running the C implementation once also fixes the issue.
    As such, it is possible that the state teardown during lifetime scope
    closure is likely to be improper for reasons yet to be known.
    Nonetheless, this commit shows that there is almost identical core logic
    being translated to Rust from C.
    
    Basic features such as dynamic headers by runtime has not been
    implemented.

--- VHF-rs/src/bin/stream.rs
+++ VHF-rs/src/bin/stream.rs
@@ -4,8 +4,39 @@ use std::time::Duration;
 use vhf::runner::{Config, VHF};
 use vhf::{Error, Result};
 
-const IOCBASEWR: libc::c_int = 0xaa00;
-const TRANSFERRED_BYTES: libc::c_int = 72 | IOCBASEWR;
+fn process_binary(
+    old_bytes: usize,
+    new_bytes: usize,
+    rbbuffer: &mmap_rs::MmapMut,
+) -> std::io::Result<Vec<u8>> {
+    // if old_bytes - new_bytes > 1 << 22 {
+    //     Err(Error::...)
+    // }
+
+    // Handle potential wrap-around in the ring buffer:
+    let data: Vec<_> = if old_bytes > new_bytes {
+        log::warn!(
+            "crossed? mmap boundary: old={}, new={}",
+            old_bytes,
+            new_bytes
+        );
+        // let first_part = &rbbuffer[old_bytes..];
+        let first_part = rbbuffer.iter().skip(old_bytes).take(1 << 22 - old_bytes);
+
+        // let second_part = &rbbuffer[..new_bytes];
+        let second_part = rbbuffer.iter().take(new_bytes);
+        first_part.chain(second_part)
+    } else {
+        // let second_part = &rbbuffer[old_bytes..new_bytes];
+        let second_part = rbbuffer.iter().skip(old_bytes).take(new_bytes - old_bytes);
+        second_part.chain(rbbuffer.iter().take(0))
+    }
+    .copied()
+    // .map(u8::from_le) // can't seem to see a difference between le/be
+    .collect();
+
+    Ok(data)
+}
 
 fn main() -> Result<()> {
     let _ = log4rs::init_file("log4rs.yml", Default::default()).unwrap(); // Logger init
@@ -18,25 +49,107 @@ fn main() -> Result<()> {
     vhf.start()?;
     log::info!("VHF started");
 
+    // Open up file for writing into
+    let mut file = {
+        use std::fs::OpenOptions;
+        use std::io::BufWriter;
+        BufWriter::new(
+            OpenOptions::new()
+                .create(true)
+                .write(true)
+                .append(false)
+                .truncate(true)
+                .open("/dev/shm/rustout.bin")
+                .map_err(Error::Io)?,
+        )
+    };
+
+    // Write some header data for Python parse to work
+    {
+        use byteorder::{LittleEndian, WriteBytesExt};
+        let x: Vec<u32> = vec![
+            0xcdef0029, 0x123456ab, 0x6f632023, 0x6e616d6d, 0x696c2064, 0x203a656e, 0x6d6f682f,
+            0x69712f65, 0x62616c74, 0x6f72702f, 0x6d617267, 0x73752f73, 0x62796862, 0x2f646972,
+            0x73707061, 0x7365742f, 0x72747374, 0x206d6165, 0x2f20552d, 0x2f766564, 0x68627375,
+            0x69726279, 0x2d203064, 0x36322071, 0x35333438, 0x20363534, 0x3420732d, 0x206c2d20,
+            0x2d20622d, 0x20332076, 0x2f206f2d, 0x2f746e6d, 0x2d73616e, 0x72626966, 0x65732d65,
+            0x6e69736e, 0x30322f67, 0x35303432, 0x4d5f3631, 0x435f4644, 0x65746e69, 0x322f6863,
+            0x30343230, 0x2f393237, 0x34323032, 0x2d38302d, 0x31543530, 0x33313a36, 0x2e38303a,
+            0x35373934, 0x6c5f3634, 0x72657361, 0x6968635f, 0x4c555f70, 0x3230304e, 0x6c5f3833,
+            0x6972645f, 0x5f726576, 0x3430304d, 0x31363533, 0x68765f37, 0x515f7066, 0x5f32304f,
+            0x75635f6c, 0x305f7272, 0x5f41322e, 0x74726f70, 0x6d756e5f, 0x5f726562, 0x746e6963,
+            0x2e686365, 0x206e6962, 0x7220230a, 0x726f6365, 0x676e6964, 0x61747320, 0x203a7472,
+            0x34323032, 0x2d38302d, 0x31543530, 0x33313a36, 0x2b38303a, 0x30303830, 0x0000000a,
+        ];
+        x.into_iter()
+            .for_each(|x| file.write_u32::<LittleEndian>(x).unwrap());
+    }
 
     // try to get some data
     let mut i = 0;
-    let mut old_tbf32 = 0;
-    while i < 5 {
-        // the actual value also, does not matter, it seems
-        let tfb32 = unsafe { usb_ioctl_read(vhf.handle, 1 as *mut i32) }.unwrap();
-        let tfb32: usize = tfb32 as usize;
-        if tfb32 > old_tbf32 {
-            println!("tfb32 = {}", tfb32);
-            println!("test_ = {}", vhf.readback[tfb32]);
-            sleep(Duration::from_nanos(500));
-            old_tbf32 = tfb32;
-            i += 1;
+    let mut tfb32_old: u32 = 0;
+    let mut debug = false; // This is just for checking transfer addresses.
+    while i < 800 {
+        let tfb32 = (vhf.ioctl_next()? >> 3 << 3) as u32 % (1 << 22);
+        if tfb32 == tfb32_old {
+            sleep(Duration::from_nanos(200));
+            continue;
         }
+        // log::trace!("tfb32 = {}", tfb32);
+
+        let this_cycle: Vec<_> = if let Ok(this_cycle) =
+            process_binary(tfb32_old as usize, tfb32 as usize, &vhf.readback)
+        {
+            this_cycle
+        } else {
+            unreachable!()
+        };
+        if tfb32_old > tfb32 {
+            debug = true;
+            log::warn!(
+                "this_cycle has been obtained for tfb32_old = {} > tfb32 ={}.",
+                tfb32_old,
+                tfb32
+            )
+        };
+
+        tfb32_old = tfb32;
+
+        if debug {
+            log::warn!("tfb32_old has now been set to {}", tfb32_old);
+            debug = false;
+        };
+
+        {
+            use byteorder::{LittleEndian, WriteBytesExt};
+            // assert_eq!(this_cycle.len() % 8, 0);
+            // chunks_exact is given by >> 3 << 3
+            this_cycle.chunks_exact(8).for_each(|x| {
+                let x: [u8; 8] = x.try_into().unwrap();
+                // use from_le_bytes documentation
+                file.write_u64::<LittleEndian>(u64::from_le_bytes(x))
+                    .map_err(Error::Io)
+                    .unwrap()
+                // file.write_all(x).map_err(Error::Io).unwrap()
+            });
+            // file.write_all(this_cycle.as_slice()).map_err(Error::Io)?
+        }
+
+        // We have yet to implement number of elements being read out thus far.
+        i += 1;
     }
+
     log::info!("Run completed");
 
+    // VHF cleanup
     vhf.stop()?;
-    log::info!("Stopped");
+    log::info!("VHF Stopped");
+
+    {
+        // File cleanup
+        use std::io::Write;
+        file.flush().map_err(Error::Io)?;
+    }
+
     Ok(())
 }
