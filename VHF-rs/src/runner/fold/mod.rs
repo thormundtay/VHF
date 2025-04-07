a636877eee55f8c198f1c4bc7bc6b404f70f652b
thormundtay <105705777+thormundtay@users.noreply.github.com>
2025-04-07 17:46:40 +0800

    Add overlapping identity transform for stream
    This looks back into the previous element to determine if an overflow
    or underflow has indeed occurred. Thereafter, this allows us to sign the
    over/underflow accordingly to pass to WriteBlock for any writers to use.

--- VHF-rs/src/runner/fold/mod.rs
+++ VHF-rs/src/runner/fold/mod.rs
@@ -1,6 +1,13 @@
-use super::process::consts::{MMAP_PAGE_LEN, VHF_MMAP_WINDOW_LEN};
+use super::process::{
+    consts::{MMAP_PAGE_LEN, VHF_MMAP_WINDOW_LEN},
+    pages::MmapPage,
+};
 use super::writer::WriteBlock;
-use crate::Result;
+use crate::{
+    parser::consts::M_OVERFLOW,
+    types::{IQMTriplet, RawVHFWord},
+    Result,
+};
 use std::{cmp::Ordering, ops::Deref, rc::Rc};
 
 #[derive(Clone)]
@@ -50,6 +57,86 @@ impl StreamFold {
         StreamFold::None(StreamFoldParameters {
             func: Rc::new(identity),
             step_by: VHF_MMAP_WINDOW_LEN,
+        })
+    }
+
+    //// This is the Identity transform with roll-over checking.
+    pub(in crate::runner) fn identity_default() -> StreamFold {
+        /// Offset into each window in which all pages prior can be thought of as lookbacks into
+        /// previous windows.
+        const PAGES_START: usize = 1;
+
+        // The "transform/map" as done by this function requires a "lookback" into the previous
+        // word to determine if a rollover has occurred. As such, the 0th element has to be chosen
+        // from the idx-1th page to ensure that the 0th window returns a sign of 0 change for the
+        // 0th element in the stream.
+        fn overlapping_identity((idx, pages): <super::VHF as Iterator>::Item) -> WriteBlock {
+            if idx == 0 {
+                debug_assert!(matches!(pages[0], MmapPage::Empty));
+                debug_assert!(matches!(pages[1], MmapPage::Page(_)));
+            } else {
+                debug_assert!(matches!(pages[0], MmapPage::Page(_)));
+                debug_assert!(matches!(pages[1], MmapPage::Page(_)));
+            };
+
+            let data_iter = pages
+                .iter()
+                .skip(PAGES_START)
+                .flat_map(Deref::deref)
+                .copied();
+
+            let mut result = WriteBlock::new_from_iter(data_iter);
+
+            fn idx_and_sign_for_filter_map(
+                (idx, (a, b)): (usize, (&RawVHFWord, &RawVHFWord)),
+            ) -> Option<(usize, i8)> {
+                let IQMTriplet(_, _, a) = a.into();
+                let IQMTriplet(_, _, b) = b.into();
+                if a.abs_diff(b) >= M_OVERFLOW {
+                    match b.cmp(&a) {
+                        // The 2nd element of the window found to be less => overflow to negative
+                        Ordering::Less => Some((idx, 1)),
+                        // The 2nd element of the window found to be more => underflow to positive
+                        Ordering::Greater => Some((idx, -1)),
+                        // Safety: M_OVERFLOW check above.
+                        Ordering::Equal => unreachable!(),
+                    }
+                } else {
+                    Some((idx, 0))
+                }
+            }
+
+            use itertools::Itertools;
+            if idx == 0 {
+                // Let the 0th window be the first non-empty page's first element in the tuple 0th
+                // and first. This ensures that the enumerate method's 0th index will be return the
+                // 0 sign change.
+                result.with_overflow_from_iter(
+                    ([pages[PAGES_START].first().unwrap()])
+                        .into_iter()
+                        .chain(pages.iter().skip(PAGES_START).flat_map(Deref::deref))
+                        .tuple_windows()
+                        .enumerate()
+                        .filter_map(idx_and_sign_for_filter_map),
+                );
+            } else {
+                result.with_overflow_from_iter(
+                    pages
+                        .iter()
+                        .flat_map(Deref::deref)
+                        .tuple_windows()
+                        .skip(PAGES_START * MMAP_PAGE_LEN - 1) // Skip all but last element of 0th page
+                        .enumerate()
+                        .filter_map(idx_and_sign_for_filter_map),
+                )
+            };
+
+            result
+        }
+
+        StreamFold::Map(StreamFoldParameters {
+            func: Rc::new(overlapping_identity),
+            step_by: VHF_MMAP_WINDOW_LEN - PAGES_START,
         })
     }
 }
--- VHF-rs/src/runner/writer/mod.rs
+++ VHF-rs/src/runner/writer/mod.rs
@@ -1,23 +1,67 @@
+// mod netcdf_writer;
+// mod v1_writer;
+// mod v2_writer;
+
 use super::super::types::RawVHFWord;
+use crate::Result;
 
 /// This is the data that is passed into what will eventually be written into File.
 pub(super) struct WriteBlock {
     data: Vec<RawVHFWord>,
+    /// None type is for cases where writers aren't expected to check that m_overflow_idx exists.
+    m_overflow_idx: Option<Vec<usize>>,
+    /// None type is for cases where writers aren't expected to check that m_overflow_idx exists.
+    m_overflow_value: Option<Vec<i8>>,
 }
 
 impl Default for WriteBlock {
     fn default() -> Self {
         WriteBlock {
             data: Vec::new(),
+            m_overflow_idx: None,
+            m_overflow_value: None,
         }
     }
 }
 
 impl WriteBlock {
-    /// Creates a new [WriteBlock] that is passed onto a
+    /// Creates a new [WriteBlock] that is passed onto a Writer.
     pub(super) fn new(data: Vec<RawVHFWord>) -> Self {
         WriteBlock {
             data,
+            ..WriteBlock::default()
         }
+    }
+
+    /// Creates a new [WriteBlock] from iterator that is passed onto a Writer.
+    pub(super) fn new_from_iter(data: impl Iterator<Item = RawVHFWord>) -> Self {
+        WriteBlock {
+            data: data.collect(),
+            ..WriteBlock::default()
+        }
+    }
+
+    /// For signs of m-overflows relative to start of Writeblock data, along with the indices
+    /// relative to the start of write-block data.
+    pub(super) fn with_overflow(&mut self, index: Vec<usize>, sign: Vec<i8>) {
+        debug_assert_eq!(index.len(), sign.len());
+        self.m_overflow_idx = Some(index);
+        self.m_overflow_value = Some(sign);
+    }
+
+    /// For signs of m-overflows relative to start of Writeblock data, along with the indices
+    /// relative to the start of write-block data, as from an iterator.
+    pub(super) fn with_overflow_from_iter(
+        &mut self,
+        index_signs: impl Iterator<Item = (usize, i8)>,
+    ) {
+        index_signs.into_iter().for_each(|(idx, val)| {
+            self.m_overflow_idx
+                .get_or_insert(Vec::with_capacity(512))
+                .push(idx);
+            self.m_overflow_value
+                .get_or_insert(Vec::with_capacity(512))
+                .push(val);
+        });
     }
 }
