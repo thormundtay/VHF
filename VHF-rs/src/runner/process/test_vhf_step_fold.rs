51f1fafcc70b646449c64451fde50c92478baded
thormundtay <105705777+thormundtay@users.noreply.github.com>
2025-04-11 17:53:28 +0800

    Add unit test for overlapping identity with m-overflow
    Similar to the without m-overflow, automatic mechanism for determining
    number of empty pages in the start has yet to be done.

--- VHF-rs/src/runner/process/test_vhf_step_fold.rs
+++ VHF-rs/src/runner/process/test_vhf_step_fold.rs
@@ -10,7 +10,7 @@ use super::test_vhf::{create_arc_pages, debug_vhf_new};
 use super::*;
 use crate::types::{IQMTriplet, Polar, RawVHFWord};
 
-use std::f64::consts::TAU;
+use std::f64::consts::{PI, TAU};
 use test_log::test;
 
 /// Check that without any m-overflow, StreamFold behaves to expectation.
@@ -204,4 +204,108 @@ fn stepped_overlapping_identity_a() {
         });
 
     assert_eq!(result_overflow_idx.len(), expected_overflow_idx.len());
+}
+
+/// Check that with m-overflow, StreamFold behaves to expectation.
+#[test]
+fn stepped_overlapping_identity_b() {
+    let StreamFold::Map(params) = StreamFold::identity_default() else {
+        log::error!("Nonoverlapping windows are not StreamFold::Map variant.");
+        panic!()
+    };
+
+    let total_window_len = params.step_by;
+    let debug_vhf = debug_vhf_new(NonZeroU64::new(total_window_len as u64).unwrap());
+    let total_elements = total_window_len * MMAP_PAGE_LEN;
+    log::info!("total_elements = {}", total_elements);
+
+    // Define the signal we are testing for.
+    let ampl = 5000f64;
+    let ang_freq = TAU / (MMAP_PAGE_LEN as f64 / 2. + 1.);
+    let phase_offset = 1.2f64;
+    let signal_phase =
+        (0..total_elements).map(|i| ampl * (i as f64).mul_add(ang_freq, phase_offset).sin());
+    let signal_radius = 5000f64;
+    let signal = signal_phase.map(|x| Polar {
+        radius: signal_radius,
+        phase: x + (TAU * 0x7FFF as f64),
+    });
+
+    // Add signal into pages.
+    // We now add data into the buffer.
+    if let Ok(mut buf) = debug_vhf.buffer.lock() {
+        buf.push_back(MmapPage::Empty);
+        let tmp_signal: Vec<_> = signal.clone().map(|x| x.into()).collect();
+        log::info!(
+            "Number of pages placed into buffer = {}",
+            create_arc_pages(&tmp_signal).len()
+        );
+        create_arc_pages(&tmp_signal)
+            .into_iter()
+            .for_each(|x| buf.push_back(x));
+    } else {
+        log::error!("Could not get log in debug_vhf.buffer");
+        panic!();
+    };
+
+    // We also need to test for sign overflow.
+    let results: Vec<_> = debug_vhf
+        .into_iter()
+        .step_by(params.step_by)
+        .map(|x| (*params.func)(x))
+        .collect();
+    let result_phase: Vec<RawVHFWord> = results
+        .iter()
+        .cloned()
+        .flat_map(|x| x.data.into_iter())
+        .collect();
+    let result_overflow_idx: Vec<_> = results.into_iter().flat_map(|x| x.overflow()).collect();
+
+    let expected_phase: Vec<RawVHFWord> = signal.map(|x| x.into()).collect();
+    let expected_overflow_idx: Vec<(usize, i8)> = {
+        // + signs
+        let plus = (1..)
+            .map(|i| (TAU * i as f64 - phase_offset) / ang_freq)
+            .take_while(|&i| i < total_elements as f64)
+            .map(|idx| (idx.ceil() as usize, 1));
+        // - signs
+        let minus = (0..)
+            .map(|i| (TAU * i as f64 + PI - phase_offset) / ang_freq)
+            .take_while(|&i| i < total_elements as f64)
+            .map(|idx| (idx.ceil() as usize, -1));
+        let mut result: Vec<(usize, i8)> = plus.chain(minus).collect();
+        result.sort_by(|a, b| a.0.cmp(&b.0));
+        result
+    };
+
+    {
+        let (i, sign) = result_overflow_idx.first().unwrap();
+        let show: Vec<IQMTriplet> = (i - 1..=i + 1).map(|i| result_phase[i].into()).collect();
+        log::info!(
+            "Elements around the first sign change ({}) are: {:?}",
+            sign,
+            show
+        );
+        let (i, sign) = result_overflow_idx.iter().skip(1).next().unwrap();
+        let show: Vec<IQMTriplet> = (i - 1..=i + 1).map(|i| result_phase[i].into()).collect();
+        log::info!(
+            "Elements around the second sign change ({}) are: {:?}",
+            sign,
+            show
+        );
+    }
+
+    assert_eq!(result_phase.len(), expected_phase.len());
+    result_phase
+        .into_iter()
+        .zip(expected_phase)
+        .for_each(|(r, e)| {
+            let IQMTriplet(ri, rq, rm) = r.into();
+            let IQMTriplet(ei, eq, em) = e.into();
+            assert_eq!(ri, ei);
+            assert_eq!(rq, eq);
+            assert_eq!(rm, em);
+        });
+
+    assert_eq!(result_overflow_idx, expected_overflow_idx);
 }
