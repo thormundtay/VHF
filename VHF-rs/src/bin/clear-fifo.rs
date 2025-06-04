360ecf809bbf578b2e0ab0ef5954bffd75a0806a
thormundtay <105705777+thormundtay@users.noreply.github.com>
2025-06-04 10:59:23 +0800

    Add show_all_dev_symlinks for current directory
    This prints all symlinks in the CWD currently and checks if the symlink
    is in /dev, and if so, if the expected device is connected. Needs
    modification for recursive traversal into entire CWD.

--- VHF-rs/src/bin/clear-fifo.rs
+++ VHF-rs/src/bin/clear-fifo.rs
@@ -1,3 +1,4 @@
+use prettytable::{Attr, Cell, Row, Table, color, row};
 use std::path::PathBuf;
 use vhf::runner::board::{Board, find_device_by_sys};
 use vhf::{Error, Result};
@@ -18,6 +19,53 @@ fn clear_fifo_per_board(board: PathBuf, hybrid_clear: bool) -> Result<()> {
     if !board.valid_interface_perms()? {
         log::warn!("VHF drivers weren't installed as user but under sudo. Please reinstall.");
     }
+
+    Ok(())
+}
+
+/// Gets all symlinks in the current folder and show if the /dev/usbhybridX the symlink is pointing
+/// to is connected.
+fn show_all_dev_symlinks() -> Result<()> {
+    let curr_dir = std::env::current_dir().map_err(Error::Io)?;
+
+    let points_to_dev = curr_dir
+        .read_dir()
+        .map_err(Error::Io)?
+        .filter_map(|entry| entry.ok().map(|e| e.path()))
+        .filter(|path| path.is_symlink())
+        .filter_map(|sym| {
+            let canon = sym.canonicalize();
+            if let Ok(true) = canon.map(|e| e.starts_with("/dev")) {
+                let p = sym.strip_prefix(curr_dir.clone()).unwrap().to_path_buf();
+                let c = unsafe { sym.canonicalize().unwrap_unchecked() };
+                let e = c.exists();
+                Some((p, c, e))
+            } else {
+                None
+            }
+        });
+
+    let mut tbl = Table::new();
+    tbl.set_titles(row!["Path", "Device", "Connected"]);
+    points_to_dev
+        .into_iter()
+        .map(|t| {
+            let p = Cell::new(t.0.as_os_str().to_str().unwrap());
+            Row::new(vec![
+                if !t.2 {
+                    p.with_style(Attr::BackgroundColor(color::BRIGHT_RED))
+                } else {
+                    p
+                },
+                Cell::new(t.1.as_os_str().to_str().unwrap()),
+                Cell::new(if t.2 { &"true" } else { &"false" }),
+            ])
+        })
+        .for_each(|r| {
+            tbl.add_row(r);
+        });
+
+    tbl.printstd();
 
     Ok(())
 }
