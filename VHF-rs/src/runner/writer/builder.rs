562ac169c9a7a2fc825b117a313594ba0dc5438e
thormundtay <105705777+thormundtay@users.noreply.github.com>
2025-06-23 16:43:32 +0800

    Add runtime selection of file-writer from user specification
    A huge match statement determines the appropriate file writer (or
    errors!) to be used.
    
    Closes #23.

--- VHF-rs/src/runner/writer/builder.rs
+++ VHF-rs/src/runner/writer/builder.rs
@@ -31,24 +31,117 @@ pub struct WriterBuilder<'a> {
 }
 
 impl<'a> WriterBuilder<'a> {
+    /// Determine (and possibly reject) based on [Config], the relevant file writer to use.
+    fn writer_type(conf: &'a Config) -> Result<Writers<'a>> {
+        // Prioritize by decreasing verbosity followed by save_to_file.
+        match conf {
+            Config {
+                verbosity: 16.., ..
+            } => {
+                log::error!("verbosity >= 16 is not supported.");
+                Err(Error::User)
+            }
+            Config {
+                num_files: 0..,
+                save_to_file: true,
+                verbosity: 4..=15,
+                ..
+            } => {
+                unimplemented!("V2Writer not yet implemented.")
+            }
+            Config {
+                num_files: 0..=1,
+                save_to_file: false,
+                verbosity: 4..=7,
+                ..
+            } => {
+                unimplemented!("V2Stdout not yet implemented.")
+            }
+            Config {
+                num_files: 2..,
+                save_to_file: false,
+                verbosity: 4..=7,
+                ..
+            } => {
+                log::error!("V2 stdout conflicts with num_files > 1.");
+                Err(Error::User)
+            }
+            Config {
+                save_to_file: false,
+                verbosity: 8..,
+                ..
+            } => {
+                log::error!(
+                    "Verbosity = 8 demands Seekable on file writer, which cannot be done with Stdout. Consider writing to file instead."
+                );
+                Err(Error::User)
+            }
+            Config {
+                num_files: 0..,
+                save_to_file: true,
+                verbosity: 0..=3,
+                encode: Encode::Binary,
+                ..
+            } => Ok(Writers::V1(V1Arg {
+                num_samples: &conf.num_samples,
+                num_files: &conf.num_files,
+                encode: &conf.encode,
+                verbosity: &conf.verbosity,
+                file_timespan: Box::new(conf.file_timespan()),
+                header_details: conf.details(),
+                filename_details: conf.filename(),
+                save_dir: &conf.save_dir,
+            })),
+            Config {
+                num_files: 0..,
+                save_to_file: true,
+                verbosity: 0..=3,
+                encode: Encode::Hexadecimal | Encode::ASCII,
+                ..
+            } => {
+                log::error!("Non-binary encoding not yet implemented.");
+                Err(Error::User)
+            }
+            Config {
+                num_files: 0..=1,
+                save_to_file: false,
+                verbosity: 0..=3,
+                encode: Encode::Binary,
+                ..
+            } => Ok(Writers::V1Stdout(V1StdOutArg {
+                encode: &conf.encode,
+                verbosity: &conf.verbosity,
+                header_details: conf.details(),
+            })),
+            Config {
+                num_files: 0..=1,
+                save_to_file: false,
+                verbosity: 0..=3,
+                encode: Encode::Hexadecimal | Encode::ASCII,
+                ..
+            } => {
+                log::error!("Non-binary encoding not yet implemented.");
+                Err(Error::User)
+            }
+            Config {
+                num_files: 2..,
+                save_to_file: false,
+                verbosity: 0..=3,
+                ..
+            } => {
+                log::error!("V1 stdout conflicts with num_files > 1.");
+                Err(Error::User)
+            }
+        }
+    }
+
     /// This is for producing [Self::build], as `./stream` requires conf to determine the
     /// file_writer type, whilst also having to time_start to already be known.
     ///
     /// This is currently a separate impl to separate out the logic from Config; but the end user
     /// should only just have to pass &conf to [Config]::writer(&self, time_start) -> impl VHFWriter
     pub(crate) fn new(conf: &'a Config) -> Result<Self> {
-        // let writer_type: Writers = WriterBuilder::writer_type(conf)?;
-        // Default to V1Arg first;
-        let writer_type = Writers::V1(V1Arg {
-            num_samples: &conf.num_samples,
-            num_files: &conf.num_files,
-            encode: &conf.encode,
-            verbosity: &conf.verbosity,
-            file_timespan: Box::new(conf.file_timespan()),
-            header_details: conf.details(),
-            filename_details: conf.filename(),
-            save_dir: &conf.save_dir,
-        });
+        let writer_type: Writers = WriterBuilder::writer_type(conf)?;
         Ok(Self {
             start_time: None,
             writer_type,
