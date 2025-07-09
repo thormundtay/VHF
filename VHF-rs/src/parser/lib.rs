585c40a0654fbf516d724493afff7b57a05b7a20
thormundtay <105705777+thormundtay@users.noreply.github.com>
2025-07-09 10:15:54 +0800

    Add absolute and relative structs for file parsing
    Also implement into_pyobject (with PyAny) type for putting into
    homogeneous HashMap to pass to GIL.

--- VHF-rs/src/parser/lib.rs
+++ VHF-rs/src/parser/lib.rs
@@ -1,1 +1,26 @@
+/// Methods for translating between Rust types to how some of our Python functions are written.
+pub mod py_binds;
+use py_binds::{RelTime, StartTime};
+
 pub mod types;
+
+/// Result associated to parsing of VHF file format.
+pub type ParseResult<T> = core::result::Result<T, ParseError>;
+
+use pyo3::PyErr;
+
+/// Errors associated during parsing of VHF file format.
+#[derive(Debug)]
+pub enum ParseError {
+    ValueError,
+    PyError(PyErr),
+    PyO3Downcast(String),
+    JiffError(jiff::Error),
+    InternalError,
+}
+
+impl From<PyErr> for ParseError {
+    fn from(value: PyErr) -> Self {
+        ParseError::PyError(value)
+    }
+}
---
+++ VHF-rs/src/parser/py_binds.rs
@@ -0,0 +1,60 @@
+use super::{ParseError, ParseResult};
+use pyo3::prelude::Python;
+use pyo3::types::PyAnyMethods;
+use pyo3::{Bound, IntoPyObject, PyAny};
+
+/// Absolute Time (Civil)
+/// Work still to be done to check if timezone is required during parsing.
+pub struct AbsTime(pub jiff::civil::DateTime);
+
+impl AbsTime {
+    pub(crate) fn into_pyobject<'py>(self, py: Python<'py>) -> ParseResult<Bound<'py, PyAny>> {
+        self.0
+            .into_pyobject(py)?
+            .downcast::<PyAny>()
+            .map_err(|e| ParseError::PyO3Downcast(e.to_string()))
+            .cloned()
+    }
+}
+
+impl From<jiff::civil::DateTime> for AbsTime {
+    fn from(value: jiff::civil::DateTime) -> Self {
+        AbsTime(value)
+    }
+}
+
+/// Relative Time
+pub struct RelTime(pub jiff::Span);
+
+impl RelTime {
+    pub(crate) fn into_pyobject<'py>(self, py: Python<'py>) -> ParseResult<Bound<'py, PyAny>> {
+        jiff::SignedDuration::try_from(self.0)
+            .map_err(ParseError::JiffError)?
+            .into_pyobject(py)?
+            .downcast::<PyAny>()
+            .map_err(|e| ParseError::PyO3Downcast(e.to_string()))
+            .cloned()
+    }
+}
+
+impl From<jiff::Span> for RelTime {
+    fn from(value: jiff::Span) -> Self {
+        Self(value)
+    }
+}
+
+/// For view window of Plot, one is free to specify either an absolute time, or time relative to
+/// the file start.
+pub enum StartTime {
+    Abs(AbsTime),
+    Rel(RelTime),
+}
+
+impl StartTime {
+    pub(crate) fn into_pyobject<'py>(self, py: Python<'py>) -> ParseResult<Bound<'py, PyAny>> {
+        match self {
+            StartTime::Abs(v) => v.into_pyobject(py),
+            StartTime::Rel(v) => v.into_pyobject(py),
+        }
+    }
+}
