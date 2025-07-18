1d50da344910a15664a884c05e72e3f07a46a581
thormundtay <105705777+thormundtay@users.noreply.github.com>
2025-07-18 14:38:15 +0800

    Add data being written out being phase checked by the Python impl
    An extremely niche bug has been caught in the Python version which
    wasn't caught using real data.

--- VHF-rs/src/runner/process/test_v1_write.rs
+++ VHF-rs/src/runner/process/test_v1_write.rs
@@ -17,6 +17,8 @@ use tempfile::TempDir;
 use test_log::test;
 
 #[cfg(feature = "o3")]
+use approx::assert_relative_eq;
+#[cfg(feature = "o3")]
 use jiff::ZonedRound;
 #[cfg(feature = "o3")]
 use std::num::NonZeroU64;
@@ -81,7 +83,7 @@ fn writes_correct_file() {
             TAU * 0x7FFF as f64,
         ),
     );
-    let _signal_expected = signal.clone(); // This will lose the engine
+    let mut signal_expected = signal.clone(); // This will lose the engine
 
     // Add signal into pages. We now add data into the buffer.
     let push_arc_pages_thread = push_arc_pages(dbg_vhf_sender, signal, Duration::new(0, 100), eng)
@@ -111,6 +113,8 @@ fn writes_correct_file() {
     {
         // Check data length
         let tmp_file = get_only_file(tmp_dir.path()).expect("Temp File not found");
+        // // copy to /dev/shm/sine.bin;
+        // std::fs::copy(&tmp_file, "/dev/shm/sine.bin").expect("failed_to copy");
         let parser = V1parser::new(&tmp_file, false).expect("Could not parse tmp file");
         parser
             .resolve_m_overflow_idxs()
@@ -149,6 +153,89 @@ fn writes_correct_file() {
             NonZeroU64::new(1u64 + Configs::default().skip_num as u64)
                 .expect("Could not read default")
         );
+
+        // Check that the reduced phase is the same.
+        let actual_reduced_phases = parser
+            .reduced_phase()
+            .expect("Could not get reduced phases");
+        assert_eq!(actual_reduced_phases.len(), total_elements);
+
+        use vhf_parse::unwrap_phase::VHFWordToUnwrappedPhaseByIter;
+        let expected_reduced_phases: Vec<_> = signal_expected.to_unwrapped_phase(0).collect();
+
+        // {
+        //     log::info!("current_dir = {:?}", std::env::current_dir().unwrap());
+        //     use plotters::prelude::*;
+        //     let root = BitMapBackend::new("plot_me.png", (640, 480)).into_drawing_area();
+        //     root.fill(&WHITE).expect("fill");
+        //
+        //     let mut chart = ChartBuilder::on(&root)
+        //         .caption("Sine phase", ("sans-serif", 50).into_font())
+        //         .margin(10)
+        //         .x_label_area_size(30)
+        //         .y_label_area_size(30)
+        //         .build_cartesian_2d(0.0..1_000.1, -34_000.1..-31_000.1)
+        //         .expect("build cart");
+        //     chart
+        //         .draw_series(LineSeries::new(
+        //             actual_reduced_phases
+        //                 .iter()
+        //                 .cloned()
+        //                 .enumerate()
+        //                 .map(|(i, v)| (i as f64, v + 1.)),
+        //             &BLUE,
+        //         ))
+        //         .expect("draw failed");
+        //     chart
+        //         .draw_series(LineSeries::new(
+        //             expected_reduced_phases
+        //                 .iter()
+        //                 .cloned()
+        //                 .enumerate()
+        //                 .map(|(i, v)| (i as f64, v)),
+        //             &RED,
+        //         ))
+        //         .expect("draw failed");
+        //
+        //     root.present().unwrap();
+        //     log::info!("saved");
+        // }
+
+        // {
+        //     use numpy::convert::ToPyArray;
+        //     use pyo3::prelude::*;
+        //     Python::with_gil(|py| {
+        //         use std::env;
+        //
+        //         let sys_module = PyModule::import(py, "sys").unwrap();
+        //         let executable_path = sys_module.getattr("path").unwrap();
+        //
+        //         // Print the executable path in Rust
+        //         log::info!("Python path: {:?}", executable_path);
+        //
+        //         let expected: Vec<_> = expected_reduced_phases.clone();
+        //         let expected = expected.to_pyarray(py);
+        //
+        //         log::info!("PYO3_PYTHON = {:?}", env::var("PYO3_PYTHON"));
+        //         let mpl = Python::import(py, "matplotlib").unwrap();
+        //         mpl.call_method1("use", ("TkAgg",)).unwrap();
+        //         let plt = Python::import(py, "matplotlib.pyplot").unwrap();
+        //         let subplots = plt.getattr("subplots").unwrap().call1((1, 1)).unwrap();
+        //         let _fig = subplots.get_item(0).unwrap();
+        //         let ax = subplots.get_item(1).unwrap();
+        //         ax.call_method1("plot", (expected,)).unwrap();
+        //         // fig.call_method1("show", (true,)).unwrap();
+        //         plt.call_method1("savefig", ("output_plot.png",)).unwrap();
+        //     });
+        // }
+
+        expected_reduced_phases
+            .into_iter()
+            .zip(actual_reduced_phases)
+            .enumerate()
+            .for_each(|(_, (e, a))| {
+                assert_relative_eq!(e, a);
+            });
     }
 
     tmp_dir.close().expect("Could not close temp_dir.");
