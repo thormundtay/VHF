9c69181d585f48897860d9f8ad7738bbf8cffe0f
thormundtay <105705777+thormundtay@users.noreply.github.com>
2025-09-15 11:11:42 +0800

    Change StreamFoldRepr to use the newer OpRepr
    The previous Representation enum was assuming too much, which OpRepr
    aimed to do better.
    
    Also changed StreamFoldRepr from a named-field struct to a tuple struct
    to perform flattening during serialization.
    
    Added a long comment about how StreamFoldRepr is currently capable of
    representing more than what StreamFoldOp is capable of doing.

--- VHF-rs/src/runner/fold/repr/mod.rs
+++ VHF-rs/src/runner/fold/repr/mod.rs
@@ -1,5 +1,9 @@
 //! For fold functions such as [super::StreamFold::identity_default] to describe what the function is
 //! doing.
+//!
+//! The high level description is currently given by [StreamFoldOpRepr::Map], which is a small
+//! wrapper of [StreamFoldMapRepr]. Populate the fields of [StreamFoldMapRepr] as necessary, which
+//! will then be written into the appropriate places of the save file.
 
 use serde::Serialize;
 
@@ -7,34 +11,36 @@ mod map;
 pub use map::*;
 
 /// This is the full description of its sibling: [StreamFoldFunction][super::StreamFoldFunction].
+///
+/// # Note
+/// While the current implementation of [StreamFoldRepr] allows for the representation of
+/// successive filtering, this is currently not implemented in the architecture around
+/// [super::StreamFoldFunction], as there still is the difficult issue of ensuring constant sized
+/// blocks of data being fed into the functions, when it is possible that the number of data being
+/// returned from the filtering functions to not always be the same.
+// Consider that the function StreamFoldFunction.func is intended to take in multiples of a page
+// worth of data at any time; this means that there might instead need to be 3 types of functions
+// in a subsequent rewrite of the function signature, being:
+// 1. <VHFIter as Iterator>::Item -> impl Iterator<Item = (usize?, ([RawVHFWord; ?], Option<(usize, i8)>?))>,
+// 2. <^1 as Iterator>::Item -> impl Iterator<Item = ^1::Item>,
+// 3. <^1 as Iterator>::Item -> impl Iterator<Item = WriteBlock>,
+// Or something along such line, along with the ability to stage the intermediate Iterators for
+// subsequent StreamFoldFunction.func filtering, whilst accounting for RPIT opaque types.
 #[derive(Clone, Default, Debug, PartialEq, Serialize)]
-pub struct StreamFoldRepr {
-    pub repr: Box<[Representation<f64>]>,
-}
+pub struct StreamFoldRepr(pub Box<[StreamFoldOpRepr<f64>]>);
 
-/// There exists a multitude of ways in which [super::super] can do process the data. This aims to pool
-/// together a collection of valid means of processing, and place the information together into a
-/// single place.
-#[derive(PartialEq, Eq, Clone, Debug, Serialize)]
-pub enum Representation<T> {
-    /// This is LFilter using a FIR. If the FIR is symmetric about some t_0, the filtering will be
-    /// be a linear phase filter.
-    FIRLfilter(FIR<T>),
-    /// This aims to represent the relevant information of the Finite Impulse Response filter, but with
-    /// time reversal double-pass, to ensure *zero-phase*, i.e.: No group-delay.
-    /// This result in a filter whose transfer function is double that specified by the filter.
-    FIRFiltFilt(FIR<T>),
-}
-
-/// This aims to represent the relevant information of the Finite Impulse Response filter.
-#[derive(PartialEq, Eq, Clone, Debug, Serialize)]
-pub struct FIR<T> {
-    /// This is the decimation performed by this FIR filter.
-    skip_num: usize,
-    /// The convolution kernel can be derived from a well-known window, in which the name should be
-    /// specified here. Otherwise, the user is free to specify their own custom window.
-    #[serde(default = "Unknown")]
-    filter_name: Option<String>,
-    /// This is the values used in convolution.
-    filter_window: Vec<T>,
+/// When defining a custom [super::StreamFold], the repr field emits a json, which will be used
+/// here to determine the resulting JSON output.
+///
+/// This currently mimics the high level idea that each processing step is either a Map or a
+/// Reduce.
+#[derive(Clone, Default, Debug, PartialEq, Serialize)]
+pub enum StreamFoldOpRepr<T>
+where
+    T: num_traits::Num,
+{
+    #[default]
+    None,
+    Map(Box<StreamFoldMapRepr<T>>),
+    // Reduce,
 }
