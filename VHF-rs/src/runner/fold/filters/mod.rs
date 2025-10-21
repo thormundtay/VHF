5676dc88722de1741a8093b9c710042cd6fa65cb
thormundtay <105705777+thormundtay@users.noreply.github.com>
2025-10-21 17:57:17 +0800

    Add documentation considerations on independence per-window filtering
    We add up to the part where it is trivial that current window that is
    being viewed is the final window, as there are pages that are marked as
    `End`.

--- VHF-rs/src/runner/fold/filters/mod.rs
+++ VHF-rs/src/runner/fold/filters/mod.rs
@@ -2,6 +2,7 @@
 #![cfg_attr(feature = "doc-images", cfg_attr(all(),
     doc = embed_doc_image::embed_image!("window_defn", "src/runner/fold/filters/images/window_defn.png"),
     doc = embed_doc_image::embed_image!("filter_wo_decimation", "src/runner/fold/filters/images/filter_wo_decimation.png"),
+    doc = embed_doc_image::embed_image!("final_window_case_a", "src/runner/fold/filters/images/final_window_case_a.png"),
 ))]
 #![cfg_attr(
     not(feature = "doc-images"),
@@ -10,11 +11,13 @@
 //!
 //! # Data from VHFIter
 //!
-//! We recall the definition for windows emitted by [VHFIter][crate::runner::VHFIter].
+//! We recall the definition for windows emitted by [VHFIter][crate::runner::VHFIter], called
+//! `VHF_MMAP_WINDOW`.
 //! ![window_defn]
 //! This is necessary as it is the first set of data taken in by a filter, before any successive
 //! [fold operations].
-//! The above is an example of 2 windows as emitted by [VHFIter][crate::runner::VHFIter].
+//! The above is an example of 2 windows as emitted by [VHFIter][crate::runner::VHFIter]. Windows
+//! are composed of contiguous [Pages][crate::runner::process::pages::MmapPage].
 //!
 //! - <details><summary><code>Pad</code>: Number of overlapping pages.</summary>  
 //!   This is the amount of overlap of data between windows of successive `vhf_iter_idx`.
@@ -49,7 +52,9 @@
 //! 1. The number of pages in a window emitted by [VHFIter][crate::runner::VHFIter].
 //! 2. Not knowing if the current page being processed by the filter function is the final window,
 //!    one must not discard otherwise valid data towards the tail end of the window (i.e.: the
-//!    final page).
+//!    final page).  
+//!    The number of pages is determined at configuration time, as set
+//!    [here][crate::runner::process::VHF::total_pages_to_read].
 //! 3. Since almost every window is not the final window, how to extract data from the body of the
 //!    window without duplication, accounting for the previous window's of data having been
 //!    "exhuasted."
@@ -75,7 +80,41 @@
 //!
 //! Let us diagramatically represent the filtering process across a data slice straddling the
 //! boundary of two non-empty pages.
+//!
 //! ![filter_wo_decimation]
+//!
+//! As a FIR Lfilter is causal, the diagram shows the result of each convolved window as being in
+//! the index of the final convolution window's index. To avoid unnecessary confusion, we describe
+//! the convolution window against the filter as being a context window. (Furthermore, context is a
+//! better nomenclature, as not everything that will be done, will necessarily be convolution.)
+//!
+//! Here, the FIR Lfilter convolution kernel of order 6 is applied for every valid index, as is
+//! represented by the context window repeated whilst sliding to the right.  
+//! For the convolution kernel in a fixed index, data elements are correspondingly multiplied with
+//! the kernel elements, before being summed to obtain the final value in the `output` array. This
+//! is diagramatically represented by the red data point in the output and how it's derived from
+//! the related Lfilter convolution kernel and data points. Note that the index written into the
+//! `output` array is true up to a constant offset, whose value is dependent on how the convolution
+//! kernel handles the data in page 0 index ~0. This is discussed in greater detail later.
+//!
+//! Duly note that the `(VHF_)MMAP_WINDOW` taken in for filtering at this step is done independently
+//! of every other filtering step. As such, the only valid assumption is that there exists previous
+//! `MMAP_WINDOW` as processed by the same function (in a possibly different thread). There is no
+//! guarantee that there exists a next `MMAP_WINDOW`, as it is entirely possible that the current
+//! `MMAP_WINDOW` is the last `MMAP_WINDOW` emitted by `VHF`. As such, output from the context
+//! convolution process on overlapping `pages` towards the back of the `MMAP_WINDOW` should be
+//! emitted into `out` for file writing. Exercise appropriate care if there final pages of
+//! `MMAP_WINDOW` are [End][crate::runner::process::pages::MmapPage::End], which indicates that the
+//! current `MMAP_WINDOW` for filtering is indeed the final `MMAP_WINDOW`.  
+//! In other words, observe the definition of `VHF_MMAP_WINDOW` as provided in [the previous
+//! section][sec:data_from_vhf_iter] as is provided in a streaming fashion, and determine how one
+//! should process the data with as minimal assumptions as possible. Diagramatically, this would
+//! correspond to  
+//! - Case 1: ![final_window_case_a]
+//!   Here, the window has a page that is explicitly declared as
+//!   [empty][crate::runner::process::pages::MmapPage::End] towards the back of the window, and
+//!   thus is trivial.
 //!
 //! [fold operations]: https://en.wikipedia.org/wiki/Fold_(higher-order_function)
 //! [decimation]: https://en.wikipedia.org/wiki/Downsampling_(signal_processing)
+//! [sec:data_from_vhf_iter]: #data-from-vhfiter
